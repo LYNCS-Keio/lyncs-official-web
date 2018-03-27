41f755f2c3f9508d62cd52a9f364b5b897452222
kn1cht <ito.turukasiwa@gmail.com>
2018-03-28 02:37:06 +0900

    renew cover images

--- js/astro.js
+++ js/astro.js
@@ -1,5 +1,5 @@
 $("document").ready(function(){
   $("#bg_image").bgswitcher({
-    images: ["../img/astro_bg01.jpg","../img/astro_bg02.jpg","../img/astro_bg03.jpg"]
+    images: ["../img/astro_bg01.jpg","../img/astro_bg02.jpg","../img/astro_bg03.jpg","../img/astro_bg04.jpg","../img/astro_bg05.jpg"]
   });
 });
